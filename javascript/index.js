a882c3320d201b187191c9f38d09d7c7adc2cc0b
SantiagoVillares <svillares@udesa.edu.ar>
2022-11-15 17:30:41 -0300

    busqueda

---
+++ javascript/index.js
@@ -0,0 +1,23 @@
+//formularios
+let url=  `https://api.themoviedb.org/3/movie/76341?api_key=2a3601e42fea0b8cec36fb4c1999c023&query=${Srch}`
+fetch(url)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        let informacion= data.results;
+        let container= document.querySelector('.res_busq');
+        let pelicula= '';
+
+        for(let i=0;i<informacion.length;i++){
+            pelicula += `<article>
+                            <img src=${informacion[i].poster_path} alt="Error" />
+                            <a href='detalle.html?id=${informacion[i].title}'>${informacion[i].title}</a>
+                        </article>`
+        }
+        container.innerHTML= pelicula;
+    })
+
+    .catch(function(error){
+        console.log(error);
+    })
--- javascript/search-results.js
+++ javascript/search-results.js
@@ -1,6 +1,6 @@
 let query = location.search;
 let StO= new URLSearchParams (query);
-let Srch= StO.get('q');
+let Srch= StO.get('querys');
 console.log(query)
 let url= `https://api.themoviedb.org/3/movie/76341?api_key=2a3601e42fea0b8cec36fb4c1999c023&query=${Srch}`
 
@@ -17,9 +17,8 @@ fetch(url)
 
         for(let i=0;i<informacion.length;i++){
             pelicula += `<article>
-                            <img src=${informacion[i].} alt='' />
-                            <a href='detalle.html?id=${}'><p>
-                            <p>
+                            <img src=${informacion[i].poster_path} alt="Error" />
+                            <a href='detalle.html?id=${informacion[i].title}'>${informacion[i].title}</a>
                         </article>`
         }
         container.innerHTML= pelicula;
