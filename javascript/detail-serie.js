74b7f34b09081b9164673db21c3bbdcdb794b6da
santinog19 <gallis@udesa.edu.ar>
2022-11-18 12:18:59 -0300

    boton

--- javascript/detail-serie.js
+++ javascript/detail-serie.js
@@ -15,7 +15,8 @@ fetch(serie)
         let contenedor_2=""
         contenedor_1+=`<h1 class="titulo">${detail.name}</h1>
                         <img class="foto_detalles" src=https://image.tmdb.org/t/p/w500/${detail.poster_path} alt="Error"/>
-                        <h2 class="slogan_txt">${detail.tagline}</h2>`
+                        <h2 class="slogan_txt">${detail.tagline}</h2>
+                        <button class ='logins'> favorito</button>`
 
         contenedor_2+=`<h2 class="subtitulo">Datos Importantes</h2>
                         <h2 class="subtitulo">Resumen</h2>
