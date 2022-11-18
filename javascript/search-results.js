90c43159191a7c1b6aa13305e473543644a9a04f
jgottblieb04 <jgottlieb@udesa.edu.ar>
2022-11-18 11:46:49 -0300

    buscador

--- javascript/search-results.js
+++ javascript/search-results.js
@@ -1,36 +1,61 @@
-let query = location.search;
-let StO= new URLSearchParams (query);
-let Srch= StO.get('querys');
-console.log(query)
-let url= `https://api.themoviedb.org/3/search/movie?api_key=2a3601e42fea0b8cec36fb4c1999c023&language=en-US&page=1&include_adult=false
-&query=${Srch}`
+let query = location.search; //Obtengo la QS
+let stringToObject = new URLSearchParams(query); //La trasnformo en OL
+let aBuscar = stringToObject.get('querys'); //Obtengo los datos de una propiedad con get()
 
-console.log(url);
 
+let url = `https://api.themoviedb.org/3/search/movie?query=${aBuscar}&api_key=2a3601e42fea0b8cec36fb4c1999c023&language=en-US&page=1&include_adult=false`
+let url2=`https://api.themoviedb.org/3/search/tv?query=${aBuscar}&api_key=2a3601e42fea0b8cec36fb4c1999c023&language=en-US&page=1&include_adult=false`
 fetch(url)
     .then(function(response){
         return response.json();
     })
     .then(function(data){
         console.log(data);
-        let informacion= document.querySelector(".resultados")
-        let container= document.querySelector('.res_busq');
-        if (data.results.length==0){
-            informacion.innerHTML+=`La busqueda ${busqueda} no encontro ningun resultado`
-        })
-        else{
-            for(let i=0;i<data.results.length;i++){
-                let img= `https://image.tmdb.org/t/p/w500${path}`
-                let path=data.results[i].poster_path
-                let id =data.results[i].id
-                console.log(id)
-                container.innerHTML+=`article class= "peli_buscada">
-                <a href=".detalle.html?id=${id}"><img src="${img}" alt="Foto${data.results[i].original_title}
-                <p class="title">${data.results[i].original_title}</p>
-                <p> ${data.results[i].release_date}</p>
-            </article>`})
-    })
+        let info = data.results
+        let container = document.querySelector('.elementos_hijos');
+        let characters = '';
 
+        for(let i=0; i<info.length; i++){
+            characters += `<article>
+                                <a href="./detail-movie.html?id=${info[i].id}">
+                                    <img src="https://image.tmdb.org/t/p/w500${info[i].poster_path}" alt="poster path movie">
+                                    <p>Name: ${info[i].title}</p>
+                                </a>
+                            </article>`
+        }
+        container.innerHTML = characters;
+
+        
+    })
+        
     .catch(function(error){
         console.log(error);
-        })
+    })
+
+
+    fetch(url2)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+        let info = data.results
+        let container = document.querySelector('.elementos_hijos');
+        let characters = '';
+
+        for(let i=0; i<info.length; i++){
+            characters += `<article>
+                                <a href="./detail-serie.html?id=${info[i].id}">
+                                    <img src="https://image.tmdb.org/t/p/w500${info[i].poster_path}" alt="poster path movie">
+                                    <p>Name: ${info[i].title}</p>
+                                </a>
+                            </article>`
+        }
+        container.innerHTML = characters;
+
+        
+    })
+        
+    .catch(function(error){
+        console.log(error);
+    })
