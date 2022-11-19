6a6c39c745ea4055c000435c4fa4181dae5428e5
santinog19 <gallis@udesa.edu.ar>
2022-11-19 18:53:47 -0300

    favoritos

--- javascript/favorite.js
+++ javascript/favorite.js
@@ -2,13 +2,13 @@ window.addEventListener("load", function () {
     let peliculasGuardadas = JSON.parse(localStorage.getItem("id"));
     console.log(peliculasGuardadas);
   
-    let container = document.querySelector(".listaDes");
+    let container = document.querySelector(".lista_Des");
     let pelicula = " ";
   
-    pelicula.innerHTML += `<li class="listaf">
-          <a class= "boton_peli" href="./detail-movie.html"> <img class="amg" src="${peliculasGuardadas[0].imagenUrl}" alt="Que Paso Ayer?"></a>
-          <a class="botones_titulos" href="./detail-movie.html"><h2>${peliculasGuardadas[0].titulo}</h2></a>
-      </li>`;
+    pelicula.innerHTML += ` <li class="listaf">
+    <a  class= "boton_peli" href="./detail-serie.html"> <img class="amg"src="${peliculasGuardadas[0].imagenUrl}" alt=""></a>
+    <a class="botones_titulos"href="./detail-serie.html"><h2> ${peliculasGuardadas[0].titulo}</h2></a>
+</li> `;
   
     container.innerHTML(pelicula);
   });
