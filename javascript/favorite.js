25a62696f57f959b4450146ab4bde55c5e505c26
santinog19 <gallis@udesa.edu.ar>
2022-11-19 19:25:05 -0300

    fav

--- javascript/favorite.js
+++ javascript/favorite.js
@@ -3,12 +3,28 @@ window.addEventListener("load", function () {
     console.log(peliculasGuardadas);
   
     let container = document.querySelector(".lista_Des");
-    let pelicula = " ";
-  
-    pelicula.innerHTML += ` <li class="listaf">
-    <a  class= "boton_peli" href="./detail-serie.html"> <img class="amg"src="${peliculasGuardadas[0].imagenUrl}" alt=""></a>
-    <a class="botones_titulos"href="./detail-serie.html"><h2> ${peliculasGuardadas[0].titulo}</h2></a>
-</li> `;
-  
-    container.innerHTML(pelicula);
-  });
+   
+    for(let i=0; i<peliculasGuardadas.length; i++){
+        favoritosmuestra(peliculasGuardadas[i])
+    }
+    function favoritosmuestra(id){
+        let url= `https://api.themoviedb.org/3/movie/${id}?api_key=2a3601e42fea0b8cec36fb4c1999c023&language=en-US`
+        fetch (url)
+            .then(function(response){
+                return response.json()
+            })
+            .then(function(data){
+                console.log(data)
+                container.innerHTML += ` <li class="listaf">
+                                <a  class= "boton_peli" href="./detail-serie.html"> <img class="amg"src=" https://image.tmdb.org/t/p/w500${peliculasGuardadas[0].imagenUrl}" alt=""></a>
+                                <a class="botones_titulos"href="./detail-serie.html"><h2> ${peliculasGuardadas[0].titulo}</h2></a>
+                            </li> `
+            })
+            .catch(function(e){
+                console.log(e)
+            })
+        }
+    })
+
+
+             
