c21dc0fdd344bb275a66ea84669454ac18ae8d77
SantiagoVillares <svillares@udesa.edu.ar>
2022-11-19 18:31:26 -0300

    casi terminadas las busquedas

--- javascript/search-results.js
+++ javascript/search-results.js
@@ -1,6 +1,8 @@
 let query = location.search; //Obtengo la QS
 let stringToObject = new URLSearchParams(query); //La trasnformo en OL
+
 let aBuscar = stringToObject.get('querys'); //Obtengo los datos de una propiedad con get()
+
 
 
 let url = `https://api.themoviedb.org/3/search/movie?query=${aBuscar}&api_key=2a3601e42fea0b8cec36fb4c1999c023&language=en-US&page=1&include_adult=false`
@@ -12,16 +14,18 @@ fetch(url)
     .then(function(data){
         console.log(data);
         let info = data.results
-        let container = document.querySelector('.elementos_hijos');
+        let container = document.querySelector('.lista_elementos_1');
         let characters = '';
+        let titulo= document.querySelector('.titulos_seccion');
+        let factos= 'Se busco: ';
+        let final= factos + `${aBuscar}`
+        titulo.innerHTML = final
 
         for(let i=0; i<info.length; i++){
-            characters += `<article>
-                                <a href="./detail-movie.html?id=${info[i].id}">
-                                    <img src="https://image.tmdb.org/t/p/w500${info[i].poster_path}" alt="poster path movie">
-                                    <p>Name: ${info[i].title}</p>
-                                </a>
-                            </article>`
+            characters += `<li class="elementos_hijos">
+                                <a class="boton_peli" href="./detail-movie.html?id=${info[i].id}"><img class="amg"src="https://image.tmdb.org/t/p/w500${info[i].poster_path}" alt="poster path movie"></a>
+                                <a class="botones_titulos" href="./detail-movie.html?id=${info[i].id}"><p class="botones_titulos">${info[i].title}</p></a>
+                            </li>`
         }
         container.innerHTML = characters;
 
@@ -39,16 +43,14 @@ fetch(url)
     .then(function(data){
         console.log(data);
         let info = data.results
-        let container = document.querySelector('.elementos_hijos');
+        let container = document.querySelector('.lista_elementos_2');
         let characters = '';
 
         for(let i=0; i<info.length; i++){
-            characters += `<article>
-                                <a href="./detail-serie.html?id=${info[i].id}">
-                                    <img src="https://image.tmdb.org/t/p/w500${info[i].poster_path}" alt="poster path movie">
-                                    <p>Name: ${info[i].title}</p>
-                                </a>
-                            </article>`
+            characters += `<li class="elementos_hijos">
+                                <a class="boton_peli" href="./detail-serie.html?id=${info[i].id}"><img class="amg"src="https://image.tmdb.org/t/p/w500${info[i].poster_path}" alt="poster path movie"></a>
+                                <a class="botones_titulos" href="./detail-serie.html?id=${info[i].id}"><p class="botones_titulos">${info[i].name}</p></a>
+                            </li>`
         }
         container.innerHTML = characters;
 
