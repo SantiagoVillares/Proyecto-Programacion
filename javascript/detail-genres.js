a5794c50e630957d25e87d59011b3c24899c6120
santinog19 <gallis@udesa.edu.ar>
2022-11-23 10:42:55 -0300

    detalles

--- javascript/detail-genres.js
+++ javascript/detail-genres.js
@@ -1,12 +1,11 @@
-let qsto= new URLSearchParams(location.search);
+let q=location.search
+let qsto= new URLSearchParams(q);
+console.log(qsto)
 let id= qsto.get("id");
 
 let type= qsto.get('type');
 let movie=`https://api.themoviedb.org/3/movie/${id}?api_key=2a3601e42fea0b8cec36fb4c1999c023&language=en-US`
 let serie= `https://api.themoviedb.org/3/tv/${id}?api_key=2a3601e42fea0b8cec36fb4c1999c023&language=en-US`
- 
-let imag = document.querySelector('.lista_elementos_1')
-let titulo=document.querySelector('.titulo_seccion_genre')
 let url_detalle_gen=`https://api.themoviedb.org/3/discover/movie?api_key=2a3601e42fea0b8cec36fb4c1999c023&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate&with_genres=${id}`
         fetch(url_detalle_gen)
             .then(function(response){
@@ -16,7 +15,7 @@ let url_detalle_gen=`https://api.themoviedb.org/3/discover/movie?api_key=2a3601e
                 console.log(data)
                 let listaGeneros = data.results
                 console.log(listaGeneros);
-                let imag = document.querySelector('.lista_elementos_5')
+                let imag = document.querySelector('.lista_elementos_genre')
                 let titulo=document.querySelector('.titulo_seccion')
                 let escritura=''
                 
@@ -24,15 +23,12 @@ let url_detalle_gen=`https://api.themoviedb.org/3/discover/movie?api_key=2a3601e
                 for(let i = 0; i<listaGeneros.length; i++){
                     escritura+=
 
-                    `<h2 class="titulos_seccion"></h2>
-    
-                    <ul class="lista_elementos_genre">
-                        <li class="elementos_hijos">
+                    `  <li class="elementos_hijos">
                             <a class="boton_peli" href="./detail-movie.html?id=${listaGeneros[i].id}"><img class="amg" src=https://image.tmdb.org/t/p/w500/${listaGeneros[i].poster_path} alt="Error" /></a>
                             <a class="botones_titulos" href="./detail-movie.html?id=${listaGeneros[i].id}"><p>${listaGeneros[i].title}</p></a>
                             
-                        </li>
-                    </ul>`
+                        </li>`
+                
                      
 
                      
